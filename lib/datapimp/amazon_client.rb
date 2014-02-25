0eb88fd65349a891264b08939f7909ea8413be92
Ankur Agarwal <signup.ankur@gmail.com>
2014-02-26 02:45:15 +0530

    Fixing the bug in the naming

--- lib/datapimp/amazon_client.rb
+++ lib/datapimp/amazon_client.rb
@@ -1,35 +1,37 @@
 require "aws-sdk"
 
-module Datapimp::AmazonClient
-  mattr_accessor :access_key_id, :secret_access_key
+module Datapimp
+  module AmazonClient
+    mattr_accessor :access_key_id, :secret_access_key
 
-  require 'datapimp/amazon_client/sts'
+    require 'datapimp/amazon_client/sts'
 
-  def self.access_key_id
-    @@access_key_id || Datapimp.profile.amazon_access_key_id || ENV['ACCESS_KEY_ID']
-  end
+    def self.access_key_id
+      @@access_key_id || Datapimp.profile.amazon_access_key_id || ENV['ACCESS_KEY_ID']
+    end
 
-  def self.secret_access_key
-    @@secret_access_key || Datapimp.profile.amazon_secret_access_key || ENV['SECRET_ACCESS_KEY']
-  end
+    def self.secret_access_key
+      @@secret_access_key || Datapimp.profile.amazon_secret_access_key || ENV['SECRET_ACCESS_KEY']
+    end
 
-  def self.configured?
-    !!(access_key_id.present? && secret_access_key.present? && (@configured || configure))
-  end
+    def self.configured?
+      !!(access_key_id.present? && secret_access_key.present? && (@configured || configure))
+    end
 
-  def self.configure
-    result = AWS.config({
-      :access_key_id => access_key_id,
-      :secret_access_key => secret_access_key
-    })
+    def self.configure
+      result = AWS.config({
+        :access_key_id => access_key_id,
+        :secret_access_key => secret_access_key
+      })
 
-    @configured = true
+      @configured = true
 
-    result
-  end
+      result
+    end
 
-  def self.temporary_s3 options={}
-    sts = AmazonClient::STS.new
-    AWS::S3.new(sts.credentials)
+    def self.temporary_s3 options={}
+      sts = AmazonClient::STS.new
+      AWS::S3.new(sts.credentials)
+    end
   end
 end
